215262f01994ef8adaac8c7ba0a805a5bb2eb662
addMoreScript <faraday@nxt.ru>
2023-07-30 13:34:52 +0400

    Обновление сайта

--- blocks/dl-dd-dt.php
+++ blocks/dl-dd-dt.php
@@ -1,7 +1,7 @@
 <div class="numblockitems">
 <?php while (have_rows('dl-dt-dd')) : the_row(); ?>
   <dl class="numblockitems__item">
-    <dt class="numblockitems__numb"><?=get_row_index() + 1; ?></dt>
+    <dt class="numblockitems__numb"><?=get_row_index(); ?></dt>
     <dt class="numblockitems__title"><?=get_sub_field('dt'); ?></dt>
     <dd><?=get_sub_field('dd'); ?></dd>
   </dl>
